05fd57e0a02458df4f1f8c20cac05a17ed2358fb
Tammy Ma <tma8520@gmail.com>
2018-04-20 17:58:06 -0400

    pearson

--- myrex.py
+++ myrex.py
@@ -2,8 +2,8 @@ import argparse
 import csv
 import sys
 import pandas as pd
-from scipy.spatial.distance import euclidean, cityblock, cosine
-from scipy.stats import pearsonr
+from scipy.spatial.distance import euclidean, cosine
+from scipy.stats import stats
 
 
 #Print results function
@@ -18,6 +18,26 @@ def output(command, train, algorithm, k, userid, movieid, prediction):
 	
 	return
 
+
+def compute_weights(sim_weights, movie_rating):
+	sim_weights = sorted(sim_weights.items(), key=lambda x: x[1], reverse=True)[:args.k]
+	movie_rating = movie_rating.loc[movie_rating['userid'].isin([i[0] for i in sim_weights])]
+	predicted_rating = 0.0
+	weights_sum = 0.0
+	
+	usr_count=0
+	for rating in movie_rating.iterrows():
+		weight = [i[1] for i in sim_weights][usr_count]
+		usr_weight = [i[0] for i in sim_weights][usr_count]
+		rate = movie_rating.loc[movie_rating["userid"]==usr_weight]["rating"].values[0]
+		predicted_rating += rate * weight
+		weights_sum += weight
+		usr_count+=1
+	
+	predicted_rating /= weights_sum
+	output(args.command, args.training_file, args.algorithm, args.k, args.user_id, args.movie_id, predicted_rating)
+	
+	
 #Get input .csv file, px, and results filename
 parser = argparse.ArgumentParser()
 parser.add_argument("command", nargs='?', help="command")
@@ -34,53 +54,58 @@ if args.command=="predict" and len(vars(args))!=6:
 elif args.command=="predict" and len(vars(args))==6:	
 	df = pd.read_csv(args.training_file, delim_whitespace=True, header=None, dtype={'ID': object}, names=["userid", "movieid", "rating", "timestamp"])
 
+	
 # user id | item id | rating | timestamp
+df_user = df.loc[df['userid'] == args.user_id][["rating", "movieid"]]
+movie_rating = df.loc[df['movieid'] == args.movie_id]
+
 
 if args.algorithm=="average":
 	df = df.loc[df['movieid'] == args.movie_id]
-	print(df["rating"].mean())
+	predicted_rating = df["rating"].mean()
+	output(args.command, args.training_file, args.algorithm, args.k, args.user_id, args.movie_id, predicted_rating)
+	
 elif args.algorithm=="euclid":
 	sim_weights = {}
-	table = {}
-	df_user = df.loc[df['userid'] == args.user_id][["rating", "movieid"]]
-	df_ids = df.userid.unique()
-	movie_rating = df.loc[df['movieid'] == args.movie_id]
-	
 	for i in movie_rating["userid"]:
 		if i!=args.user_id:
 			df_other = df.loc[(df['userid'] == i)][["rating", "movieid"]]
 			df_both = pd.merge(df_user, df_other, on='movieid', how='inner', suffixes=(args.user_id, i))
 			df_both = df_both.dropna()
-			print(df_both)
 			dist = euclidean(df_both.loc[:,("rating" + str(args.user_id))].values.ravel(), df_both.loc[:,("rating" + str(i))].values.ravel())
 			sim_weights[i] = 1.0 / (1.0 + dist)
 		
-	print(sim_weights)
-	
-	sim_weights = sorted(sim_weights.items(), key=lambda x: x[1], reverse=True)[:args.k]
-	movie_rating = movie_rating.loc[movie_rating['userid'].isin([i[0] for i in sim_weights])]
-	predicted_rating = 0.0
-	weights_sum = 0.0
+	print(sim_weights)	
+	compute_weights(sim_weights, movie_rating)
+
+elif args.algorithm=="pearson":
+	sim_weights = {}
+	df_user = df.loc[df['userid'] == args.user_id][["rating", "movieid"]]
+	#Normalize target user ratings
+	df_user["rating"] = (2*(df_user["rating"] - df_user["rating"].min()) - (df_user["rating"].max() - df_user["rating"].min())) / (df_user["rating"].max() - df_user["rating"].min())
+	print(df_user)
+	movie_rating = df.loc[df['movieid'] == args.movie_id]
 	
 	usr_count=0
-	for rating in movie_rating.iterrows():
-		weight = [i[1] for i in sim_weights][usr_count]
-		print(weight)
-		usr_weight = [i[0] for i in sim_weights][usr_count]
-		rate = movie_rating.loc[movie_rating["userid"]==usr_weight]["rating"].values[0]
-		print(rate)
-		predicted_rating += rate * weight
-		weights_sum += weight
-		usr_count+=1
-	
-	predicted_rating /= weights_sum
-	output(args.command, args.training_file, args.algorithm, args.k, args.user_id, args.movie_id, predicted_rating)
-	
-
-#elif args.algorithm=="pearson":
+	for j in movie_rating["userid"]:
+		if j!=args.user_id:
+			df_other = df.loc[(df['userid'] == j)][["rating", "movieid"]]
+			df_other["rating"] = (2*(df_other["rating"] - df_other["rating"].min()) - (df_other["rating"].max() - df_other["rating"].min())) / (df_other["rating"].max() - df_other["rating"].min())
+			df_both = pd.merge(df_user, df_other, on='movieid', how='inner', suffixes=(args.user_id, j))
+			df_both = df_both.fillna(0)
 			
-
-
+			dist = stats.pearsonr(df_both.loc[:,("rating" + str(args.user_id))].values.ravel(), df_both.loc[:,("rating" + str(j))].values.ravel())
+			sim_weights[j] = 1.0 / (1.0 + dist[0])
+	print(sim_weights)		
+	compute_weights(sim_weights, movie_rating)
+	
+elif args.algorithm=="cosine":
+	sim_weights = {}
+	df_user = df.loc[df['userid'] == args.user_id][["rating", "movieid"]]
+	#Normalize target user ratings
+	df_user["rating"] = (2*(df_user["rating"] - df_user["rating"].min()) - (df_user["rating"].max() - df_user["rating"].min())) / (df_user["rating"].max() - df_user["rating"].min())
+	print(df_user)
+	movie_rating = df.loc[df['movieid'] == args.movie_id]
 	
 
 	
